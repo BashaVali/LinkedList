37fbc9a87bc422aa58cf15e1680494f3b8aaa640
BashaVali <bashavali222@gmail.com>
2022-12-13 10:15:35 +0530

    Insertion At Last

---
+++ LinkedList/Program.cs
@@ -0,0 +1,40 @@
+﻿using LinkedList;
+using System;
+namespace LinkedListStack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedLists linkedList = new LinkedLists();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("***Linked List Problem ***");
+
+                Console.WriteLine(" Select the option ");
+
+                Console.WriteLine("1.CreateLinkedList\n" +
+                                  "2.Display" + "\n" +
+                                  "3.Exit" + "\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linkedList.Display();
+                        break;
+                    case 2:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+
+            }
+        }
+    }
+}
